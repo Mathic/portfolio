84bad1a5ffa97e342ba7e5090a85731e61905d3f
Riza Baltazar <rizbalt@gmail.com>
2019-05-03 16:55:27 -0400

    Node.js and sass install

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,6 +30,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'info.apps.InfoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
