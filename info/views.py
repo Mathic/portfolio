2ce22e535eb4f328cbe20099d1fef662c4aba3cc
Riza Baltazar <rizbalt@gmail.com>
2019-05-09 12:13:13 -0400

    Responsiveness, pages: about, projects and contact

--- info/views.py
+++ info/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'info/index.html')
+    return render(request, 'info/index.html', {'nbar': 'home'})
     # return HttpResponse("Hello, world! You're in the info index.")
 
 def about(request):
-    return render(request, 'info/about.html')
+    return render(request, 'info/about.html', {'nbar': 'about'})
 
 def projects(request):
-    return render(request, 'info/projects.html')
+    return render(request, 'info/projects.html', {'nbar': 'projects'})
 
 def contact(request):
-    return render(request, 'info/contact.html')
+    return render(request, 'info/contact.html', {'nbar': 'contact'})
