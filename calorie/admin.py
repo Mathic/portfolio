5d0b723b70f251168fb68d6f70fba9a312862086
Riza Baltazar <rizbalt@gmail.com>
2019-05-15 12:44:58 -0400

    Health app database migrations

--- calorie/admin.py
+++ calorie/admin.py
@@ -5,4 +5,6 @@ from .models import *
 # Register your models here.
 admin.site.register(Setup)
 admin.site.register(Entry)
+admin.site.register(Calorie)
+admin.site.register(Sleep)
 admin.site.register(Exercise)
