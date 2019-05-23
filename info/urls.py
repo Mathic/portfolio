4cdbab8e608d689c0fdfa39be4c93e5a38de64da
Riza Baltazar <rizbalt@gmail.com>
2019-05-22 22:42:26 -0400

    Projects and info home link
    - Link to info page was being sent to health page
    - Added health page to projects

--- info/urls.py
+++ info/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.info, name='info'),
     path('about', views.about, name='about'),
     path('projects', views.projects, name='projects'),
     path('contact', views.contact, name='contact'),
--- info/views.py
+++ info/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def info(request):
     return render(request, 'info/index.html', {'nbar': 'home'})
     # return HttpResponse("Hello, world! You're in the info index.")
 
